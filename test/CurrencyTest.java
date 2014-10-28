e5eaea3c400a3a3b2176a3f1fdccc937fbd992b1
mtronicsDev <maxi.schmeller@live.de>
2014-10-28 21:06:39 +0100

    Structured tests!

---
+++ test/CurrencyTest.java
@@ -0,0 +1,23 @@
+import com.mtronicsdev.translatorGator.Translator;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author mtronics_dev (Maxi Schmeller)
+ * @version 28.10.2014 21:04
+ */
+public class CurrencyTest {
+
+    @Test
+    public void testCurrency() {
+        Translator.initialize();
+
+        Translator.setLocale("de_DE");
+        assertEquals("9.755.345.187.399,57 \u20ac", Translator.translate("%9,755,345,187,399.5656C%")); //€-Sign
+
+        Translator.setLocale("en_US");
+        assertEquals("$9,755,345,187,399.57", Translator.translate("%9,755,345,187,399.5656C%"));
+    }
+
+}
---
+++ test/DateTimeTest.java
@@ -0,0 +1,23 @@
+import com.mtronicsdev.translatorGator.Translator;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author mtronics_dev (Maxi Schmeller)
+ * @version 28.10.2014 21:04
+ */
+public class DateTimeTest {
+
+    @Test
+    public void testDateTime() {
+        Translator.initialize();
+
+        Translator.setLocale("de_DE");
+        assertTrue(Translator.translate("%date% %time%").matches("\\d{1,2}(\\.)\\d{1,2}(\\.)\\d{4} \\d{1,2}:\\d{1,2}:\\d{1,2}"));
+
+        Translator.setLocale("en_US");
+        assertTrue(Translator.translate("%date% %time%").matches("\\p{Alpha}{3} \\d{1,2}, \\d{4} \\d{1,2}:\\d{1,2}:\\d{1,2} [A|P]M"));
+    }
+
+}
